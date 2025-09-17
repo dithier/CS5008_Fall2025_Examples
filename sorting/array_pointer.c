7d9d5973ace1524b5e3a0f15c99ec47667aadea7
dithier <carter.ithier@gmail.com>
2025-09-17 13:11:18 -0400

    add array pointer teaching ex

---
+++ sorting/array_pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void test(int arr[], int len) {
+    // print out element 2
+}
+
+void test2(int* arr, int len) {
+    // print out element 2
+}
+
+int main() {
+    // initialize int array
+
+    // print adress for each element in array
+
+    // print pointer address of array
+
+    // use pointer arithmatic to print element 2
+
+    // call test
+
+    // call test2
+
+    return 0;
+}
