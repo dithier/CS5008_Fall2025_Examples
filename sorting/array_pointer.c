0d391eafb25bb3385e0d201628b196c0438363a8
dithier <carter.ithier@gmail.com>
2025-09-17 21:03:45 -0400

    finished sorting demo

--- sorting/array_pointer.c
+++ sorting/array_pointer.c
@@ -2,24 +2,34 @@
 
 void test(int arr[], int len) {
     // print out element 2
+    printf("%d\n", arr[2]);
 }
 
 void test2(int* arr, int len) {
     // print out element 2
+    printf("%d\n", arr[2]);
 }
 
 int main() {
     // initialize int array
+    int array[] = {1, 5, 2, 10, 15};
+    int size = sizeof(array) / sizeof(array[0]);
 
     // print adress for each element in array
-
+    for (int i = 0; i < size; i++) {
+        printf("%p\n", &array[i]);
+    }
     // print pointer address of array
+    printf("%p\n", array);
 
     // use pointer arithmatic to print element 2
+    printf("%d\n", *(array + 2));
 
     // call test
+    test(array, size);
 
     // call test2
+    test2(array, size);
 
     return 0;
 }
--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -2,24 +2,46 @@
 
 void swap(int* a, int* b) {
     // todo
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void bubble_sort(int arr[], int length) {
     // todo
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
 }
 
 void print_arr(int arr[], int length) {
     // todo
+    for (int i = 0; i < length; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    printf("\n");
 }
 
 int main() {
     // initialize array
+    int arr[] = {1, 10, 3, 4, 6};
+    int size = 5;
 
     // print before array
+    printf("before:\n");
+    print_arr(arr, size);
 
     // sort
+    bubble_sort(arr, size);
 
     // print after array
+    printf("after:\n");
+    print_arr(arr, size);
 
     return 0;
 }
