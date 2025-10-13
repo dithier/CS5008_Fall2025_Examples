c62864abf7574e1e1fa03dd396f01ddb35e7d55a
dithier <carter.ithier@gmail.com>
2025-10-13 11:32:30 -0400

    add file io stuff

---
+++ files/read_data_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // create filepointer
+
+    // open file
+
+    // read data and print with feof
+
+    // close file ppointer
+
+    return 0;
+}
---
+++ files/read_data_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // create filepointer
+
+    // open file
+
+    // read data and print with fscanf
+
+    // close filepointer
+
+    return 0;
+}
