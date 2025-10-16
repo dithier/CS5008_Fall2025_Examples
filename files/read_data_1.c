5bb86d71e533b00c4bb466b2ef68dc0afabd8afe
dithier <carter.ithier@gmail.com>
2025-10-15 20:22:15 -0400

    finished reading data demo

--- files/read_data_1.c
+++ files/read_data_1.c
@@ -2,12 +2,21 @@
 
 int main() {
     // create filepointer
+    FILE *fp = NULL;
 
     // open file
+    fp = fopen("data.txt", "r");
+
+    int buffer = 0;
 
     // read data and print with feof
+    while (!feof(fp)) {
+        fscanf(fp, "%d", &buffer);
+        printf("%d\n", buffer);
+    }
 
     // close file ppointer
+    fclose(fp);
 
     return 0;
 }
--- files/read_data_2.c
+++ files/read_data_2.c
@@ -2,12 +2,19 @@
 
 int main() {
     // create filepointer
+    FILE *fp = fopen("data.txt", "r");
 
     // open file
 
+    int buffer = 0;
+
     // read data and print with fscanf
+    while (fscanf(fp, "%d", &buffer) == 1) {
+        printf("%d\n", buffer);
+    }
 
     // close filepointer
+    fclose(fp);
 
     return 0;
 }
